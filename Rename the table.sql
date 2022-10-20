a8004951443c64a77a99e9c1f741f677c1bdd8b6
Saulen <saulenhilpakar@gmail.com>
2022-10-21 00:00:13 +0545

    add file

---
+++ Rename the table.sql
@@ -0,0 +1,6 @@
+--Rename table
+
+Alter table table_name rename to new_table_name;
+
+--careful while altering the big table it might take a very long time
+
---
+++ insert into table.sql
@@ -0,0 +1,4 @@
+--insert into table
+
+insert into table_name select * from table_name
+
